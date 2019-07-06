1345da08190655da6259ede0c9bd1ab9cd041f47
Rvail <naveedm@tcd.ie>
2019-07-07 00:17:10 +0100

    Added confirm dialog and persist to file

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,7 +14,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -28,35 +35,79 @@ public class MainActivity extends AppCompatActivity {
 
         lvItems = findViewById(R.id.lvItems);
         items = new ArrayList<String>();
+        readItems();
         itemAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemAdapter);
-        items.add("item 1");
-        items.add("item 2");
         setupListViewListener();
     }
+
+
 
     private void setupListViewListener() {
         lvItems.setOnItemLongClickListener(
                 new AdapterView.OnItemLongClickListener() {
                     @Override
                     public boolean onItemLongClick(AdapterView<?> adapter,
-                                                   View item, int pos, long id) {
-                        // Remove the item within array at position
-                        items.remove(pos);
-                        // Refresh the adapter
-                        itemAdapter.notifyDataSetChanged();
-                        // Return true consumes the long click event (marks it handled)
+                                                   View item, final int pos, long id) {
+
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("Delete " + items.get(pos) + "?");
+                        builder.setCancelable(false);
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Remove the item within array at position
+                                items.remove(pos);
+                                // Refresh the adapter
+                                itemAdapter.notifyDataSetChanged();
+                                writeItems();
+                                // Return true consumes the long click event (marks it handled)
+
+                                Toast.makeText(getApplicationContext(), "Item deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(getApplicationContext(), "Action cancelled", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                        builder.show();
+
                         return true;
                     }
 
                 });
     }
 
-    public void onAddItem(View v){
+    public void onAddItem(View v) {
         EditText etNewItem = findViewById(R.id.etNewItem);
         String text = etNewItem.getText().toString();
         itemAdapter.add(text);
         etNewItem.setText("");
+        writeItems();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
