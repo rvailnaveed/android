e9828b45c7443fc791d4d07f28655cbf699db453
Rvail <naveedm@tcd.ie>
2019-07-07 23:32:25 +0100

    Experimenting with tabs

---
+++ Clock/app/src/main/java/com/example/clock/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.clock;
+
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener, Tab2.OnFragmentInteractionListener, Tab3.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.pager);
+        MyPagerAdapter mpa = new MyPagerAdapter(getSupportFragmentManager());
+        vp.setAdapter(mpa);
+
+        TabLayout tabLayout = findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(vp);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
---
+++ Clock/app/src/main/java/com/example/clock/MyPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.clock;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class MyPagerAdapter extends FragmentStatePagerAdapter {
+
+    public MyPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        switch (i){
+            case 0:
+                return new Tab1();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab3();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "ALARMS";
+            case 1:
+                return "TIMER";
+            case 2:
+                return "STOPWATCH";
+            default:
+                return null;
+        }
+    }
+}
